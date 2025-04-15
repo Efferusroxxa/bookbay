98549e8d08aff4c770b18d0259b6cfa9758ffa5d
Efferusroxxa <efferusroxxa632@gmail.com>
2025-04-15 18:56:03 +0600

    Initial Setup plus routing done

---
+++ src/pages/BlogDetails/BlogDetails.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BlogDetails = () => {
+    return (
+        <div>
+            This is Blog Details page
+        </div>
+    );
+};
+
+export default BlogDetails;
---
+++ src/pages/ProductAuthor/ProductAuthor.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProductAuthor = () => {
+    return (
+        <div>
+            This is Author page
+        </div>
+    );
+};
+
+export default ProductAuthor;
