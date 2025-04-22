7047b7249966a52ac5c420ebb05ab6fe0ce4a14b
Efferusroxxa <efferusroxxa632@gmail.com>
2025-04-22 21:54:08 +0600

    our all tings are updated and header top and middle part is done

--- src/pages/TestComponents/TestComponents.jsx
+++ src/pages/TestComponents/TestComponents.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "../../Components/Header/Header";
+import Button from "../../Components/Button/Button";
 
 const TestComponents = () => {
-    return (
-        <div className="container mx-auto my-20 shadow">
-        <Header></Header>
-        </div>
-    );
+  return (
+    <div className="container mx-auto my-20 shadow">
+    {/* <Button variant="primary">Hello World</Button> */}
+    <Header></Header>
+  </div>
+  );
 };
 
 export default TestComponents;
